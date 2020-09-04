f828e2ae0aa436e1d5aab1e1088ab3cb2601020c
Chan <norules.prateek@gmail.com>
2020-09-04 14:14:57 +0530

    merged message item into text item

--- app/src/main/java/com/example/firy/customrecycle/TextMessageItem.kt
+++ app/src/main/java/com/example/firy/customrecycle/TextMessageItem.kt
@@ -1,20 +1,50 @@
 package com.example.firy.customrecycle
 
 import android.content.Context
+import android.view.Gravity
+import android.widget.FrameLayout
 import com.example.firy.R
 import com.example.firy.customrecycle.items.MessageItem
 import com.example.firy.model.TextMessage
+import com.google.firebase.auth.FirebaseAuth
 import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
+import com.xwray.groupie.kotlinandroidextensions.Item
 import kotlinx.android.synthetic.main.item_text_message.*
+import org.jetbrains.anko.wrapContent
+import java.text.SimpleDateFormat
 
 class TextMessageItem(
     val message: TextMessage,
     val context: Context
-) : MessageItem(message){
+) : Item(){
 
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-       viewHolder.textViewMessageItem.text = message.text
-        super.bind(viewHolder, position)
+        viewHolder.textViewMessageItem.text = message.text
+        setTimeText(viewHolder)
+        setMessageRootGravity(viewHolder)
+    }
+
+    private fun setTimeText(viewHolder: GroupieViewHolder){
+        val dateFormat = SimpleDateFormat
+            .getDateTimeInstance(SimpleDateFormat.SHORT, SimpleDateFormat.SHORT)
+        viewHolder.textViewMessageTimeItem.text = dateFormat.format(message.time)
+    }
+
+    private fun setMessageRootGravity(viewHolder: GroupieViewHolder){
+        if(message.senderId == FirebaseAuth.getInstance().currentUser?.uid){
+            viewHolder.message_root.apply {
+                backgroundResource = R.drawable.rect_round_black
+                val lParams = FrameLayout.LayoutParams(wrapContent, wrapContent, Gravity.END)
+                this.layoutParams = lParams
+            }
+        }
+        else{
+            viewHolder.message_root.apply {
+                backgroundResource = R.drawable.rect_round_primary_dark
+                val lParams = FrameLayout.LayoutParams(wrapContent, wrapContent, Gravity.START)
+                this.layoutParams = lParams
+            }
+        }
     }
 
     override fun isSameAs(other: com.xwray.groupie.Item<*>): Boolean {
